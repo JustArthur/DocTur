677c9133dff8cabde498e4da8332af014762ecd0
su apt install wayz <wayzzy59@gmail.com>
2023-01-11 13:40:45 +0100

    aaaa

--- pages/panel.php
+++ pages/panel.php
@@ -21,65 +21,29 @@
 
     <title>Bienvenue, </title>
 </head>
-<body>
+<body> 
 
-    <aside>
-        <div class="titre">
-            <h1>Doc<span class="degrade">Tur</span></h1>
+    <?php
+
+        include_once ('./src/sidebar.php');
+
+    ?>
+
+    <section class="global">
+        <div class="ligne">
+            <form class="search" action="" method="post">
+                <div class="div">
+                    <input type="search" name="">
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </div>
+                <input type="submit" value="Rechercher">
+            </form>
         </div>
-        <ul class="list">
-            <li class="list-item">
-                <a href="#">
-                    <div class="icon">
-                        <span class="material-symbols-rounded">dashboard</span>
-                    </div>
-                    <div class="lien">
-                        <p>Accueil</p>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <div class="icon">
-                        <span class="material-symbols-rounded">star</span>
-                    </div>
-                    <div class="lien">
-                        <p>Favoris</p>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <div class="icon">
-                        <span class="material-symbols-rounded">person</span>
-                    </div>
-                    <div class="lien">
-                        <p>Profil</p>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <div class="icon">
-                        <span class="material-symbols-rounded">settings</span>
-                    </div>
-                    <div class="lien">
-                        <p>Paramètres</p>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item btn-deco">
-                <a href="#">
-                    <div class="icon">
-                        <span class="material-symbols-rounded">logout</span>
-                    </div>
-                    <div class="lien">
-                        <p>Déconnexion</p>
-                    </div>
-                </a>
-            </li>
-        </ul>
-    </aside> 
+        <div class="ligne"></div>
+        <div class="ligne"></div>
+    </section>
     
 </body>
 </html>
