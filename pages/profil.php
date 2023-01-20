b212aa432b83e8a359453a5a972712e929f01011
su apt install wayz <wayzzy59@gmail.com>
2023-01-20 15:37:00 +0100

    aaa

--- pages/profil.php
+++ pages/profil.php
@@ -38,16 +38,6 @@
 
     <section class="global">
         <div class="barre-recherche">
-            <form class="search" action="" method="post">
-                <div class="div">
-                    <input type="search" name="search" placeholder="Rechercher un fichier ou un dossier ..." class="barre">
-                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                    </svg>
-                    <a href="profil"><img src="<?= $avatar ?>" alt=""></a>
-                </div>
-            </form>
-
             <div class="profil">
                 <div class="message">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
@@ -60,14 +50,23 @@
                         <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
                     </svg>
                 </div>
-                <div class="avatar">
-                    <img src="<?= $avatar ?>" alt="">
-                </div>
             </div>
         </div>
 
         <div class="compte">
             <h1 class="titre">Mon profil</h1>
+            <div class="banniere">
+                <img src="<?= $banner ?>" alt="">
+                <div class="avatar">
+                <img src="<?= $avatar ?>" alt="">
+            </div>
+            </div>
+            <div class="details">
+                <div class="pseudo"><?= $_SESSION['utilisateur'][1]?></div>
+            </div>
+            <div class="bio">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta exercitationem non nobis pariatur dolorum praesentium hic, ducimus iste deserunt sequi officia veniam ex blanditiis! Quae aut, voluptates hic ut tempora tenetur quibusdam laborum ab fugit cumque quaerat magni odio non corporis at tempore facilis libero. Voluptates eius nulla corporis. Accusantium!
+            </div>
         </div>
     </section>
     
